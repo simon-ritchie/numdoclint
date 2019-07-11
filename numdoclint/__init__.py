0f1597b7d3e058e30e49525a33cd8a0e34029a2d
simon-ritchie <antisocial.sid2@gmail.com>
2019-07-11 20:42:45 +0900

    Added interface test. Added information id constants to numdoclint module.

--- numdoclint/__init__.py
+++ numdoclint/__init__.py
@@ -1,4 +1,12 @@
 # flake8: noqa
 
-from numdoclint.py_module import (check_python_module,
-                                  check_python_module_recursively)
+from numdoclint.py_module import (
+    check_python_module, check_python_module_recursively,
+    INFO_ID_LACKED_ARGUMENT, INFO_ID_LACKED_DOCSTRING_PARAM,
+    INFO_ID_LACKED_DOCSTRING_PARAM_TYPE,
+    INFO_ID_LACKED_DOCSTRING_PARAM_DESCRIPTION,
+    INFO_ID_DIFFERENT_PARAM_ORDER, INFO_ID_LACKED_FUNC_DESCRIPTION,
+    INFO_ID_LACKED_ARG_DEFAULT_VALUE, INFO_ID_LACKED_DOC_DEFAULT_VALUE,
+    INFO_ID_LACKED_DOCSTRING_RETURN, INFO_ID_LACKED_DOCSTRING_RETURN_TYPE,
+    INFO_ID_LACKED_DOCSTRING_RETURN_DESCRIPTION,
+    INFO_ID_LACKED_RETURN_VAL)
---
+++ tests/test_numdoclint.py
@@ -0,0 +1,38 @@
+import inspect
+
+import numdoclint
+from numdoclint import py_module
+import pytest
+
+
+def test_interface():
+    interface_name_list = [
+        'check_python_module',
+        'check_python_module_recursively',
+    ]
+    for interface_name in interface_name_list:
+        assert hasattr(numdoclint, interface_name)
+        interface_func = getattr(numdoclint, interface_name)
+        assert callable(interface_func)
+
+
+def test_info_id_constants():
+    numdoclint_info_id_const_num = 0
+    members = inspect.getmembers(numdoclint)
+    for name, obj in members:
+        if not name.startswith('INFO_ID_'):
+            continue
+        if not isinstance(obj, int):
+            continue
+        numdoclint_info_id_const_num += 1
+
+    py_module_const_num = 0
+    members = inspect.getmembers(py_module)
+    for name, obj in members:
+        if not name.startswith('INFO_ID_'):
+            continue
+        if not isinstance(obj, int):
+            continue
+        py_module_const_num += 1
+
+    assert numdoclint_info_id_const_num == py_module_const_num
